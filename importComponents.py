d34468124e1248386f8060c78013ec0a9853a8a0
Loic <l.loic017@gmail.com>
2023-07-20 16:50:09 +0100

    Moving from private Repo

---
+++ importComponents.py
@@ -0,0 +1,57 @@
+import tkinter as tk
+from tkinter import filedialog
+import customtkinter as ct
+from PIL import Image, ImageTk
+
+class GraphicsFolderFrame(ct.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
+
+        self.fmPathHeading = ct.CTkLabel(self, text="STEP 1")
+        self.fmPathHeading.pack(padx=0, pady=5)
+        self.fmPathLabel = ct.CTkLabel(self, text="Select Football Manager Graphics Folder Path", fg_color="gray30", corner_radius=6)
+        self.fmPathLabel.pack(padx=20, pady=3)
+        self.inputButton = ct.CTkButton(self, text="Browse", command=self.findFolder)
+        self.inputButton.pack(padx=20, pady=20)
+
+    # Add FootballManager Path
+    def findFolder(self):
+        self.filePath = filedialog.askdirectory()
+        self.filePathLabel = ct.CTkLabel(self, text="Folder Path Picked:")
+        self.filePathLabel.pack()
+        self.filePathPath = ct.CTkLabel(self, text=self.filePath)
+        self.filePathPath.pack()
+
+class ImportTypeFrame(ct.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
+
+        self.fmPathHeading = ct.CTkLabel(self, text="STEP 2")
+        self.fmPathHeading.pack(padx=0, pady=5)
+        self.fmPathLabel = ct.CTkLabel(self, text="Select Import Type", fg_color="gray30", corner_radius=6)
+        self.fmPathLabel.pack(padx=20, pady=3)
+
+        self.importTypeVar = tk.StringVar()
+        self.logoRadioButton = ct.CTkRadioButton(self, text="Team Logo Import", variable=self.importTypeVar, value="logo")
+        self.logoRadioButton.pack(padx=20, pady=3)
+        self.playerRadioButton = ct.CTkRadioButton(self, text="Player Image Import", variable=self.importTypeVar, value="player")
+        self.playerRadioButton.pack(padx=20, pady=15)
+
+class UploadImageFrame(ct.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
+
+        self.fmPathHeading = ct.CTkLabel(self, text="STEP 3")
+        self.fmPathHeading.pack(padx=0, pady=5)
+        self.fmPathLabel = ct.CTkLabel(self, text="Select Image to Import" , fg_color="gray30", corner_radius=6)
+        self.fmPathLabel.pack(padx=20, pady=3)
+        self.inputButton = ct.CTkButton(self, text="Browse", command=self.findFolder)
+        self.inputButton.pack(padx=20, pady=20)
+
+    # Import Image
+    def findFolder(self):
+        self.filePath = filedialog.askopenfilename()
+        self.filePathLabel = ct.CTkLabel(self, text="File Picked:")
+        self.filePathLabel.pack(padx=10, pady=10)
+        self.imagePathLabel = ct.CTkLabel(self, text=self.filePath)
+        self.imagePathLabel.pack(padx=10, pady=10)
---
+++ main.py
@@ -0,0 +1,96 @@
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import messagebox
+import customtkinter as ct
+from PIL import Image, ImageTk
+from importComponents import GraphicsFolderFrame, ImportTypeFrame, UploadImageFrame 
+import os
+
+# Finish Import Frame
+class FinishImportFrame(ct.CTkFrame):
+    def __init__(self, master, graphicsFolder, importType, filePath):
+        super().__init__(master)
+        # Other Frames - Type Class
+        self.graphicsFolder = graphicsFolder
+        self.importType = importType
+        self.filePath = filePath
+
+        self.finishStepLabel = ct.CTkLabel(self, text="STEP 4")
+        self.finishStepLabel.pack(padx=0, pady=5)
+
+        # User input ID
+        self.idLabel = ct.CTkLabel(self, text="Enter Football Manager ID", fg_color="gray30", corner_radius=6)
+        self.idLabel.pack(padx=0, pady=5)
+        self.idInput = ct.CTkEntry(self)
+        self.idInput.pack(padx=20, pady=3)
+
+        # Button to activate processImport function
+        self.finishButton = ct.CTkButton(self, text="Import", command=self.processImport)
+        self.finishButton.pack(padx=20, pady=20)
+
+    # Process user inputs to import selected images
+    def processImport(self):
+
+        self.graphicsPath = self.graphicsFolder.filePathPath.cget("text")
+        self.importType = self.importType.importTypeVar.get()
+        self.imagePath = self.filePath.imagePathLabel.cget("text")
+        self.fmId = self.idInput.get()
+
+        image = Image.open(str(self.imagePath))
+        # Resize images to correct Football Manager dimensions
+        if self.importType == "logo":
+            normalLogo = image.resize((200, 200))
+            smallLogo = image.resize((20, 20))
+
+            # Create directory if it doesnt exist
+            normalDir = "{}/{}/teamLogo/normal/".format(str(self.graphicsPath), str(self.fmId))
+            smallDir = "{}/{}/teamlogo/small/".format(str(self.graphicsPath), str(self.fmId))
+
+            os.makedirs(normalDir, exist_ok=True)
+            os.makedirs(smallDir, exist_ok=True)
+
+            # Save Images
+            normalLogo.save("{}/{}.png".format(normalDir, str(self.fmId)), 'PNG')
+            smallLogo.save("{}/{}.png".format(smallDir, str(self.fmId)), 'PNG')
+        elif self.importType == "player":
+            playerFace = image.resize((200, 200))
+
+            # Create directory if it doesnt exist
+            normalDir = "{}/{}/playerFaces/".format(str(self.graphicsPath), str(self.fmId))
+
+            os.makedirs(normalDir, exist_ok=True)
+
+            # Save Images
+            playerFace.save("{}/{}.png".format(normalDir, str(self.fmId)), 'PNG')
+
+
+# Main App
+class App(ct.CTk):
+    def __init__(self):
+        super().__init__()
+        ct.set_appearance_mode("dark")
+        ct.set_default_color_theme("blue")
+
+        # Graphics Folder Frame
+        self.graphicsFolder = GraphicsFolderFrame(self)
+        self.graphicsFolder.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="nsew")
+
+        # Select Import Type
+        self.importType = ImportTypeFrame(self)
+        self.importType.grid(row=1, column=0, padx=10, pady=(10, 0), sticky="nsew")
+
+        # Import Image Frame
+        self.uploadImage = UploadImageFrame(self)
+        self.uploadImage.grid(row=1, column=1, padx=10, pady=(10, 0), sticky="nsew")
+
+        # Finish Import
+        self.finishImport = FinishImportFrame(self, self.graphicsFolder, self.importType, self.uploadImage)
+        self.finishImport.grid(row=2, column=0, padx=10, pady=(10, 0), sticky="nsew")
+
+        # app = ct.CTk()
+        self.geometry("650x650")
+        self.title("Football Manager Importer")
+
+# Run App
+app = App()
+app.mainloop()
